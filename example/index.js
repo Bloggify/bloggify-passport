1b1cb9fda2c48000dead9752e93d0a8e5420ce9c
Ionică Bizău <bizauionica@gmail.com>
2017-06-30 12:45:35 +0300

    :arrow_up: 1.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -1,11 +1,13 @@
-...
-"bloggify-passport": {
-    login_url: "/user/signin",
-    verify_callback (email, password, cb) {
-        if (email === "foo@bar.com" && password === "42") {
-            return cb(null, { name: "foo" })
+{
+    //...
+    "bloggify-passport": {
+        login_url: "/user/signin",
+        verify_callback (email, password, cb) {
+            if (email === "foo@bar.com" && password === "42") {
+                return cb(null, { name: "foo" })
+            }
+            cb(new Error("Invalid username or password."))
         }
-        cb(new Error("Invalid username or password."))
     }
-},
-...
+    //...
+}
