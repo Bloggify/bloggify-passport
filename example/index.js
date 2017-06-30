c26e41477bd00931a8a5ae5215958549b77767bd
Ionică Bizău <bizauionica@gmail.com>
2017-06-30 12:31:58 +0300

    :arrow_up: 1.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -1,5 +1,11 @@
-"use strict";
-
-const bloggifyPassport = require("../lib");
-
-console.log(bloggifyPassport());
+...
+"bloggify-passport": {
+    login_url: "/user/signin",
+    verify_callback (email, password, cb) {
+        if (email === "foo@bar.com" && password === "42") {
+            return cb(null, { name: "foo" })
+        }
+        cb(new Error("Invalid username or password."))
+    }
+},
+...
